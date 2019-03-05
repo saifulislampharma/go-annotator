ae0c4eb7ae3746e96ad323a4cc0c9c0bea6a6b17
Saiful <saiful_islampharmadu@yahoo.com>
2019-03-05 19:21:37 +0600

    Update model_cc.py

--- src/model_cc.py
+++ src/model_cc.py
@@ -9,7 +9,7 @@ from keras import regularizers
 from keras.layers.advanced_activations import PReLU
 from . import data_preparation_cc as data_preparations
 
-#nb_classes = 29621
+
 nb_classes = data_preparations.NO_CLASSES
 
 
@@ -19,8 +19,7 @@ def build_model():
     
     a = Dense(50, activation='relu')(b)
     a = Lambda(lambda x:K.expand_dims(x, axis = -1))(a)
-    #s1 = int_shape(a)
-    #a = Reshape((s1[1], s1[2], 1))(a)
+ 
 
     a1 = a
     x1 = ZeroPadding2D(padding = (3//2, 0)) (a1)
@@ -45,8 +44,7 @@ def build_model():
   
     # concatenating 3 filters
     x_cnn = concatenate([ x1, x2, x3 ])
-    #x=add([x1,x2,x3])
-    #x_cnn = BatchNormalization()(x4)
+
 
     # RNN network
     x_rnn = Bidirectional(GRU(300 ,activation='relu',return_sequences = True), merge_mode='concat')(x_cnn)
@@ -61,21 +59,7 @@ def build_model():
     x = Dropout(0.5)(x)
     x = Dense(nb_classes, activation ='sigmoid')(x)
 
-    """
-    #x=Bidirectional(LSTM(75 ,activation='relu',return_sequences=True),merge_mode='sum')(x)
-    #s3=int_shape(x)
-    #x=Reshape((s3[-1],1,s3[1]))(x)
-    #x=Reshape((1,s3[1],s3[-1]))(x)
-    #x=concatenate([x,x4])
-    #x=GlobalAveragePooling2D()(x)
-    x=Flatten()(x4)
-    x=BatchNormalization()(x)
-    x=Dense(512,activation='relu')(x)
-    x=BatchNormalization()(x)
-    x=Dense(128,activation='relu')(x)
-    #x=Dropout(0.6)(x)
-    #x = Dense(nb_classes, activation ='sigmoid')(a)
-    """
+ 
 
     return Model(inputs = b, outputs = x)
 
